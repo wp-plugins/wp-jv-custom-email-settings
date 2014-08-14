4f4f5d7c85487a80ad33f979650048b7a4f510ed
janosver <janosver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-14 03:30:22 +0000

    Initial release (1.0).
    git-svn-id: https://plugins.svn.wordpress.org/wp-jv-custom-email-settings/trunk@965513 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,46 @@
+=== WP JV Custom Email Settings ===
+Contributors: Janos Ver
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=JNF92QJY4PGGA&lc=HU&item_name=WP%20JV%20Custom%20Email%20Settings%20%2d%20Plugin%20Donation&item_number=2&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Tags: admin, administration, email, newsletter, notification, simple, wordpress
+Requires at least: 3.9.2
+Tested up to: 3.9.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Let you change default "Wordpress <wordpress@yoursite.com>" e-mail address when sending out e-mails
+
+== Description ==
+
+By default when you send any notification from your blog (including many of the Plugins as well) sender is "Wordpress" and the sender e-mail address is <wordpress@yourdomain.com>. I wanted to have personalized sender name and e-mail address that is why I created this Plugin. 
+
+This plugin will enable you to 
+* Replace default <wordpress@yourdomain.com> to any e-mail address
+* Replace default e-mail from "Wordpress"
+
+== Installation ==
+
+1. Download wp-jv-custom-email-settings.zip
+2. Extract to `/wp-content/plugins/wp-jv-custom-email-settings` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Where can I define new "Email From" name and from "Email Address"? =
+
+Go to Settings -> General -> "WP Custom Email Settings" section
+
+
+== Screenshots ==
+
+No screenshots yet.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+* Initial release.
---
+++ wp-jv-custom-email-settings.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name: WP JV Custom Email Settings
+ * Plugin URI: http://janosver.com/projects/wordpress/wp-jv-custom-email-settings
+ * Description: By default all notification emails received from "Wordpress" wordpress@yourdomain.com. Once this plugin activated you can customize these (email from text and email address) at Settings -> General -> "WP JV Custom Email Settings" section
+ * Version: 1.0
+ * Author: Janos Ver
+ * License: GPLv2 or later
+ */
+
+ 
+//No direct access allowed to plugin php file
+if(!defined('ABSPATH')) {
+	die('You are not allowed to call this page directly.');
+}
+  
+// Adds Settings link to Plugin page next under Plugin description
+function wp_jv_ces_settings_link($links, $file) {
+	if ( strpos( $file, 'wp-jv-custom-email-settings.php' ) !== false ) {
+	$new_links = array(
+						'<a href="options-general.php#wp_jv_ces_set_email_from">Settings</a>'
+					  );
+	
+	$links = array_merge( $links, $new_links );
+	}
+return $links;
+}
+add_filter( 'plugin_row_meta', 'wp_jv_ces_settings_link' , 10, 2 );
+
+
+// Adds Donate link to Plugin page next under Plugin description 
+function wp_jv_prg_donate_link($links, $file) {
+	if ( strpos( $file, 'wp-jv-custom-email-settings.php' ) !== false ) {
+	$new_links = array(
+						'<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=JNF92QJY4PGGA&lc=HU&item_name=WP%20JV%20Custom%20Email%20Settings%20%2d%20Plugin%20Donation&item_number=2&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">Donate</a>'
+					  );
+	
+	$links = array_merge( $links, $new_links );
+	}
+return $links;
+}
+add_filter( 'plugin_row_meta', 'wp_jv_prg_donate_link' , 10, 2 );
+
+
+//Initialize settings
+add_action( 'admin_init', 'wp_jv_ces_admin_init' );
+ 
+//Adding settings to Settings->General
+function wp_jv_ces_admin_init() {
+	add_settings_section('wp_jv_ces_general_settings','WP JV Custom Email Settings','wp_jv_ces_settings','general');
+	add_settings_field('wp_jv_ces_set_email_from', 'Email From', 'wp_jv_ces_set_email_from', 'general', 'wp_jv_ces_general_settings');
+	add_settings_field('wp_jv_ces_set_email_from_address', 'Email Address', 'wp_jv_ces_set_email_from_address', 'general', 'wp_jv_ces_general_settings');
+	register_setting( 'wp_jv_ces_general_settings', 'wp_jv_ces_set_email_from' );
+	register_setting( 'wp_jv_ces_general_settings', 'wp_jv_ces_set_email_from_address' );
+}
+
+//WP JV Custom Email Settings section intro text
+function wp_jv_ces_settings() {  
+	echo 'By default all notification emails received from "Wordpress" < wordpress@'. str_replace('https://www.','',str_replace('http://www.','',str_replace('https://','',str_replace('http://','',site_url())))). ' >. You can change these below.';
+}
+
+//Settings field to set Email From text
+function wp_jv_ces_set_email_from() {
+	settings_fields( 'wp_jv_ces_general_settings' ); 	
+	echo '<input class="regular-text ltr" type="text" id="wp_jv_ces_set_email_from" name="wp_jv_ces_set_email_from" placeholder="Wordpress" value="'. get_option('wp_jv_ces_set_email_from'). '"></input>';
+} 
+
+//Settings field to set Email from email Address
+function wp_jv_ces_set_email_from_address() {
+	settings_fields( 'wp_jv_ces_general_settings' ); 	
+	echo '<input class="regular-text ltr" type="email" id="wp_jv_ces_set_email_from_address" name="wp_jv_ces_set_email_from_address" placeholder="wordpress@'. str_replace('https://www.','',str_replace('http://www.','',str_replace('https://','',str_replace('http://','',site_url())))). '" value="'. get_option('wp_jv_ces_set_email_from_address'). '"></input>';
+}
+
+//Replace default <wordpress@yourdomain.com> e-mail address 
+function wp_jv_ces_wp_mail_from($email){
+	return get_option('wp_jv_ces_set_email_from_address');
+}
+//Overwrite default e-mail address only if user set new value
+if (get_option('wp_jv_ces_set_email_from_address')) { add_filter('wp_mail_from', 'wp_jv_ces_wp_mail_from');}
+
+//Replace default e-mail from "Wordpress"
+function wp_jv_ces_wp_mail_from_name($from_name){
+	return get_option('wp_jv_ces_set_email_from');
+}
+//Overwrite default e-mail from text only if user set new value
+if (get_option('wp_jv_ces_set_email_from')) { add_filter('wp_mail_from_name', 'wp_jv_ces_wp_mail_from_name');}
+
+?>

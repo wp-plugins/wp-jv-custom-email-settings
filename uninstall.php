f4f9eb6fc9bbd422ed94824162eb7bec6dd5bc9d
janosver <janosver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-19 09:05:28 +0000

    v1.4 - Previous release caused another saving issue which is fixed now
    git-svn-id: https://plugins.svn.wordpress.org/wp-jv-custom-email-settings/tags/1.4@968181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Remove settings on plugin delete.
+ *
+ * WP JV Custom Email Settings Uninstaller
+ * @version 1.4
+ */
+
+if(!defined('WP_UNINSTALL_PLUGIN')) {
+  die('You are not allowed to call this page directly.');
+}
+
+delete_option('wp_jv_ces_general_settings');
+delete_option('wp_jv_ces_set_email_from');
+delete_option('wp_jv_ces_set_email_from_address');
+
+?>

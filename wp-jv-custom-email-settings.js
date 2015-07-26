b5734631c10c9e9d3e87b4e2dbde856bf0a6eadd
janosver <janosver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-26 15:08:58 +0000

    v2.3 * Performance improvement by using minified js code * Bulk subscribe/unsubscribe users to/from e-mail notifications (go to Users->All Users and see the new bulk actions) * Hungarian translation updated
    git-svn-id: https://plugins.svn.wordpress.org/wp-jv-custom-email-settings/tags/2.3@1206850 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-jv-custom-email-settings.js
@@ -0,0 +1,69 @@
+/*
+// Scripts for WP JV Custom Email Settings
+// @version: 2.2
+*/
+jQuery(document).ready(function($){
+	/************************************************************************************************************/
+	/* Invoke notification sending */
+	/************************************************************************************************************/	    
+	$('#btnSendNotification').click(function(){	
+		//Disable button	
+		$('#btnSendNotification').attr('disabled', true);				
+	
+		//Display loading icon while it is sending out emails
+		document.getElementById('dProgress').style.display = 'inline-block';		
+		document.getElementById('jv-ces-message').style.display = 'inline-block';	
+		document.getElementById('jv-ces-message').innerHTML=wp_jv_ces_obj.sending_mails;		
+				
+		$('#dProgress')
+			.ajaxStart(function() {								
+				$(this).show();
+			})
+			.ajaxStop(function() {
+				$(this).hide();				
+				$(this).unbind("ajaxStart"); //added to fix unnecessary turning on the loading icon by other scripts running on the page
+			})
+		;
+	
+		//Get the post id from hidden input
+		var postid = document.getElementById('jv-notification-postid').value;			
+		
+		//Send e-mails
+		data = {
+                action			: 'wp_jv_ces_send_notification_manual',
+                url				: ajaxurl,
+                type			: 'POST',
+				dataType		: 'text',
+				'post_id'		: postid,
+				wp_jv_ces_nonce	: wp_jv_ces_obj.wp_jv_ces_nonce
+            };				
+		$.post(ajaxurl, data, function(response){		
+			//to debug uncomment the following line
+			/*
+			alert (	'error:'+response.error+'\n'+
+					'error_msg:'+response.error_msg+'\n'+					
+					'logged_count: '+response.logged_count+'\n'+
+					'sent_count:'+response.sent_count+'\n'+
+					'sending_error_count:'+response.sending_error_count+'\n'+
+					'error_code:'+response.error_code+'\n'+
+					'log_page_url:'+response.log_page_url);
+			*/
+			if (response.error) {
+				if (response.logged_count==0 || response.sent_count==0) {
+					document.getElementById('jv-ces-message').innerHTML=wp_jv_ces_obj.error_sending;
+					alert (response.error_msg + '\n\n[Error: '+ response.error_code + ']');
+				}
+				else {
+					document.getElementById('jv-ces-message').innerHTML=response.sent_count + ' ' + wp_jv_ces_obj.emails_out_of + ' ' + (response.sent_count+response.sending_error_count) + ' '+wp_jv_ces_obj.sent_with+' ' + (response.sent_count+response.sending_error_count-response.logged_count) + ' '+wp_jv_ces_obj.log_issues+' <a href="' + response.log_page_url + '">' + wp_jv_ces_obj.log + '</a> '+wp_jv_ces_obj.for_details;					
+					$('#btnSendNotification').attr('value', wp_jv_ces_obj.resend);
+				}
+			} else {					
+					//Emails sent successfully					
+					document.getElementById('jv-ces-message').innerHTML='<br>' + response.sent_count + ' ' + wp_jv_ces_obj.notif_sent_check + ' <a href="' + response.log_page_url + '">'+ wp_jv_ces_obj.log + '</a> ' +wp_jv_ces_obj.for_details;
+					$('#btnSendNotification').attr('value', wp_jv_ces_obj.resend);
+					}
+			//Re-enable button
+			$('#btnSendNotification').attr('disabled', false);		
+		});				
+	});			
+});
